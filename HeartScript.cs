0b0d9a3a408b0c1613d64d76d4f7348a1ed88536
Joseph Nguyen <jngu124@lsu.edu>
2019-03-02 19:57:36 -0600

    Add files via upload
    This update includes a game timer and heart system for the player that loses one life when the guard(frog) collides with the player. One power up (extra heart) is included as well that adds one life to the player. Teleporting between scenes has also been added. When the player collides with the door (eagle), they will go to level 2. 
    Date - 3/2/19

---
+++ HeartScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartScript : MonoBehaviour
+{
+    public GameObject heartpickup;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        heartpickup.gameObject.SetActive(false);
+        GameControlScript.health += 1;
+
+    }
+}
---
+++ Scripts/Health.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public int health;
+    public int numberOfHearts;
+    public Image[] hearts;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(health > numberOfHearts)
+        {
+            health = numberOfHearts;
+        }
+
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i < health)
+            {
+                hearts[i].sprite = fullHeart;
+            }
+            else
+            {
+                hearts[i].sprite = emptyHeart;
+            }
+            if(i < numberOfHearts)
+            {
+                hearts[i].enabled = true;
+            }
+            else
+            {
+                hearts[i].enabled = false;
+            }
+        }
+    }
+
+    //void OnTriggerEnter2D(Collider2D other)
+    //{
+        // ..and if the game object we intersect has the tag 'Pick Up' assigned to it..
+        //if (other.gameObject.CompareTag("frog"))
+        //{
+            // Make the other game object (the pick up) inactive, to make it disappear
+            //other.gameObject.GetComponent<Renderer>().enabled = false;
+
+        //}
+
+    //}
+}
