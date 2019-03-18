e11fc79d1cfe9c99bc93687a8e66ef9585dae4f6
Ward Allen Leavines <35572384+Ward-47@users.noreply.github.com>
2019-03-18 15:33:16 -0500

    Add files via upload

---
+++ MenuController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public string MainMenuScene;
+    public GameObject PauseMenu;
+    public bool isPaused; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame(); 
+            }
+            else
+            {
+                isPaused = true;
+                PauseMenu.SetActive(true);
+                Time.timeScale = 0f; //freezes the game when paused 
+            }
+        }
+    }
+
+    public void ResumeGame()
+    {
+        isPaused = false;
+        PauseMenu.SetActive(false);
+        Time.timeScale = 1f; //unfreezes the game when unpaused
+    }
+
+    public void ReturnToMain()
+    {
+        Time.timeScale = 1f; //sets the game back to being unfrozen when player resumes game in the main menu. 
+        SceneManager.LoadScene(MainMenuScene);
+    }
+}
