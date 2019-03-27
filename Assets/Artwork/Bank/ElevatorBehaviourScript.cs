7af545ab40072be1307964a8755ff49bf8eb2162
alliza <bistaaliza@gmail.com>
2019-03-27 00:42:16 -0500

    level_2 design

---
+++ Assets/Artwork/Bank/ElevatorBehaviourScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorBehaviourScript : MonoBehaviour
+    
+    
+{
+    public float speed = .1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 temp = transform.position;
+        temp.y += speed * Time.deltaTime;
+        transform.position = temp;
+    }
+}
