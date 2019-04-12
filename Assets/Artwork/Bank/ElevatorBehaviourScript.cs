1f8a1744c58293df0a5a4e78b2b970134a6b286e
alliza <bistaaliza@gmail.com>
2019-04-12 15:12:12 -0500

    Elevator_Fix

--- Assets/Artwork/Bank/ElevatorBehaviourScript.cs
+++ Assets/Artwork/Bank/ElevatorBehaviourScript.cs
@@ -13,9 +13,9 @@ public class ElevatorBehaviourScript : MonoBehaviour
     {
         
     }
-    private void OnCollisionEnter2D (Collision2D collision)
-    {
-        Debug.Log(collision.collider.name);
+     private void OnCollisionEnter2D (Collision2D collision)
+     {
+        Debug.Log(collision.collider.tag);
         if (collision.collider.tag == "Player")
         {
             allowVertical = true;
@@ -24,16 +24,24 @@ public class ElevatorBehaviourScript : MonoBehaviour
             // when your current object moves, the other one follow along
 
             collision.gameObject.transform.parent = gameObject.transform;
-            
+
         }
+      
+      }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+            allowVertical = false;
+            gameObject.transform.DetachChildren();
     }
+
 
     // Update is called once per frame
     void Update()
     {
         if (allowVertical)
-        { 
+        {
             // move change the y direction of an elevator1.
+            print("Am I here");
             transform.Translate(0, Input.GetAxis("Vertical") * speed * Time.deltaTime, 0);
         }
 
